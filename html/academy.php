c5b505211f2f9251b4fe03d8e660e24710dbc3d9
DevTraveler17 <alexx.prozorov.97@mail.ru>
2019-03-21 19:29:30 +0300

    decoment cathcha

--- html/academy.php
+++ html/academy.php
@@ -12,12 +12,12 @@
     <body>
     
 <?php
-//if (isset($_POST['g-recaptcha-response'])) {
-//  $url_to_google_api = "https://www.google.com/recaptcha/api/siteverify";
-//    $secret_key = '6LeE55gUAAAAANCtGhzX05mgjMpfYbt32H9cy3_a';
-//    $query = $url_to_google_api . '?secret=' . $secret_key . '&response=' . $_POST['g-recaptcha-response'] . '&remoteip=' . $_SERVER['REMOTE_ADDR'];
-//    $data = json_decode(file_get_contents($query));
-//    if ($data->success) {
+if (isset($_POST['g-recaptcha-response'])) {
+  $url_to_google_api = "https://www.google.com/recaptcha/api/siteverify";
+    $secret_key = '6LeE55gUAAAAANCtGhzX05mgjMpfYbt32H9cy3_a';
+    $query = $url_to_google_api . '?secret=' . $secret_key . '&response=' . $_POST['g-recaptcha-response'] . '&remoteip=' . $_SERVER['REMOTE_ADDR'];
+    $data = json_decode(file_get_contents($query));
+/    if ($data->success) {
 
         //if capthcha is valid
         require_once 'connection.php';
@@ -40,15 +40,12 @@
         }
         mysqli_close($connect);
 
-//    } else {
-//        exit('Извините но похоже вы робот \(0_0)/');
-//    }
-//} else {
-//    exit('Вы не прошли валидацию reCaptcha');
-//}
-
-
+    } else {
+        exit('Извините но похоже вы робот \(0_0)/');
+    }
+} else {
+    exit('Вы не прошли валидацию reCaptcha');
+}
 ?>
-
 </body>
 </html>
